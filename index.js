5752f2a2b7024f95a8277582af7ce0462351cf84
alexDeVep <a.pavlov@koptelnya.ru>
2022-11-27 16:26:18 +0300

    - add nodemon

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
-
+app.use(express.static(path.join(__dirname, "build")));
 app.listen(3001, () => {
     console.log('server started');
 });
